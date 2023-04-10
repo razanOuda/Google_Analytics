e5a1d4c1fe2be87a120248e378630cf9ee502680
razanOuda <razan.ouda2002@gmail.com>
2023-04-10 22:16:38 +0300

    10-4

--- app/src/main/java/com/example/firestoreassignment/MainActivity.java
+++ app/src/main/java/com/example/firestoreassignment/MainActivity.java
@@ -6,15 +6,25 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,11 +33,43 @@ public class MainActivity extends AppCompatActivity {
     List<FetchData> fetchData;
     HelperAdapter helperAdapter;
     DatabaseReference databaseReference ;
+    private FirebaseAnalytics mFirebaseAnalytics;
+    Calendar c = Calendar.getInstance();
+    int hour = c.get(Calendar.HOUR_OF_DAY);
+    int minute = c.get(Calendar.MINUTE);
+    int second = c.get(Calendar.SECOND);
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        recyclerView = findViewById(R.id.note);
+        screenTrack("Home screen");
+
+
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        screenTrack("Home Screen");
+        recyclerView = findViewById(R.id.note);
+
+
+        Log.e("hour", String.valueOf(hour));
+        Log.e("minute", String.valueOf(minute));
+        Log.e("second", String.valueOf(second));
+
+        recyclerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnEvent("recycler_View", "recyclerView1", "RecyclerView");
+                btnEvent("recycler_View2", "recyclerView2", "RecyclerView");
+                btnEvent("recycler_View3", "recyclerView3", "RecyclerView");
+
+
+            }
+        });
+
 
         recyclerView = findViewById(R.id.recycler_View);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -45,6 +87,8 @@ public class MainActivity extends AppCompatActivity {
                 recyclerView.setAdapter(helperAdapter);
             }
 
+
+
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
@@ -52,5 +96,64 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+    public void btnEvent(String id, String name, String contentType) {
+        Bundle bundle = new Bundle();
+        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, id);
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, name);
+        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, contentType);
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
+    }
+
+    public void screenTrack(String name) {
+        Bundle bundle = new Bundle();
+        bundle.putString(FirebaseAnalytics.Param.SCREEN_NAME, name);
+        bundle.putString(FirebaseAnalytics.Param.SCREEN_CLASS, "Main Activity");
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SCREEN_VIEW, bundle);
+    }
+
+
+    @Override
+    protected void onPause() {
+        Calendar c = Calendar.getInstance();
+        int hour2 = c.get(Calendar.HOUR_OF_DAY);
+        int minute2 = c.get(Calendar.MINUTE);
+        int second2 = c.get(Calendar.SECOND);
+
+        int h = hour2 - hour;
+        int m = minute2 - minute;
+        int s = second2 - second;
+
+
+        Log.e("hour", String.valueOf(h));
+        Log.e("minute", String.valueOf(m));
+        Log.e("second", String.valueOf(s));
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
+        HashMap<String, Object> users = new HashMap<>();
+        users.put("hours", h);
+        users.put("minute", m);
+        users.put("second", s);
+        users.put("screenName", "Home screen");
+
+
+        db.collection("razan")
+                .add(users)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+        super.onPause();
+    }
+
 
     }
--- app/src/main/java/com/example/firestoreassignment/Secondactivity.java
+++ app/src/main/java/com/example/firestoreassignment/Secondactivity.java
@@ -1,23 +1,64 @@
 package com.example.firestoreassignment;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
 
 public class Secondactivity extends AppCompatActivity {
     HelperAdapter2 helperAdapter2;
     RecyclerView recyclerViewSecond;
+    private FirebaseAnalytics mFirebaseAnalytics;
+    Calendar c = Calendar.getInstance();
+    int hour = c.get(Calendar.HOUR_OF_DAY);
+    int minute = c.get(Calendar.MINUTE);
+    int second = c.get(Calendar.SECOND);
 
+    @SuppressLint("WrongViewCast")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_item);
+
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        recyclerViewSecond = findViewById(R.id.Secondactivity);
+        screenTrack("second screen");
+        recyclerViewSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnEvent("recycler_View", "recyclerView1", "RecyclerView");
+            }
+        });
+
+
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        screenTrack("second Screen");
+        recyclerViewSecond = findViewById(R.id.recycler_View);
+
+
+        Log.e("hour", String.valueOf(hour));
+        Log.e("minute", String.valueOf(minute));
+        Log.e("second", String.valueOf(second));
+
+
+
+
         Intent intent = this.getIntent();
         Bundle bundle = intent.getExtras();
         FetchData fetchData=(FetchData) bundle.getSerializable("key");
@@ -32,4 +73,64 @@ public class Secondactivity extends AppCompatActivity {
 
 
     }
+
+    public void btnEvent(String id, String name, String contentType) {
+        Bundle bundle = new Bundle();
+        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, id);
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, name);
+        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, contentType);
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
+    }
+
+    public void screenTrack(String name) {
+        Bundle bundle = new Bundle();
+        bundle.putString(FirebaseAnalytics.Param.SCREEN_NAME, name);
+        bundle.putString(FirebaseAnalytics.Param.SCREEN_CLASS, "Main Activity");
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SCREEN_VIEW, bundle);
+    }
+
+
+    @Override
+    protected void onPause() {
+        Calendar c = Calendar.getInstance();
+        int hour2 = c.get(Calendar.HOUR_OF_DAY);
+        int minute2 = c.get(Calendar.MINUTE);
+        int second2 = c.get(Calendar.SECOND);
+
+        int h = hour2 - hour;
+        int m = minute2 - minute;
+        int s = second2 - second;
+
+
+        Log.e("hour", String.valueOf(h));
+        Log.e("minute", String.valueOf(m));
+        Log.e("second", String.valueOf(s));
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
+        HashMap<String, Object> users = new HashMap<>();
+        users.put("hours", h);
+        users.put("minute", m);
+        users.put("second", s);
+        users.put("screenName", "second screen");
+
+
+        db.collection("razan")
+                .add(users)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+        super.onPause();
+    }
+
 }
